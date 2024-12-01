1f290599813826fbadc671e3900465a9c56fbba9
Harika0602 <159090518+Harika0602@users.noreply.github.com>
2024-12-01 17:08:15 -0500

    Update main.js

--- extra-credit/src/main.js
+++ extra-credit/src/main.js
@@ -3,13 +3,17 @@ import App from './App.vue';
 import { createRouter, createWebHistory } from 'vue-router'; // Import correct Vue Router 3 methods
 import HomePage from './components/HomePage.vue';
 import SurveyPage from './components/SurveyPage.vue';
+import ViewSurvey from './components/ViewSurvey.vue';
+import EditSurvey from './components/EditSurvey.vue';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
 // Define the routes
 const routes = [
   { path: '/', component: HomePage },
-  { path: '/survey', component: SurveyPage }
+  { path: '/survey', component: SurveyPage },
+  { path: '/survey/:id/view', component: ViewSurvey },
+  { path: '/survey/:id/edit', component: EditSurvey },
 ];
 
 // Create the router instance using the new Vue Router 3 syntax
